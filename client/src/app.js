86eba316cd3b174e01b167c52f347c5816efdf70
joannaarroyo <joliuac@gmail.com>
2019-09-25 15:51:03 -0700

    custom hook useForm created and implemented

--- client/src/app.js
+++ client/src/app.js
@@ -3,27 +3,20 @@ import React, {useState, useEffect} from 'react';
 import io from 'socket.io-client';
 import Q from '@nmq/q/client';
 
+import useForm from "./hooks/useForm";
+
 // Connect outside of the render cycle ...
 const socket = io.connect('http://localhost:3000');
 const queue = new Q('deeds');
 
 const App = (props) => {
-
-  const [values, setValues] = useState({});
+  const [values, handleChange, handleSubmit] = useForm({});
   const [queueMessage, setQueueMessage] = useState({});
   const [socketMessage, setSocketMessage] = useState({});
 
-  const handleChange = e => {
-    setValues({...values, [e.target.name]: e.target.value});
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    e.target.reset();
-
-    Q.publish('deeds', 'work', values);
-    socket.emit('words', values);
-
+  const submitCallback = (formValues) => {
+    Q.publish('deeds', 'work', formValues);
+    socket.emit('words', formValues);
   };
 
   useEffect( () => {
@@ -37,20 +30,19 @@ const App = (props) => {
 
   }, []);
 
-
   return (
     <>
       <pre>Form Values: {JSON.stringify(values)}</pre>
       <pre>Queue Values: {JSON.stringify(queueMessage)}</pre>
       <pre>Socket Values: {JSON.stringify(socketMessage)}</pre>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={(event) => handleSubmit(event, submitCallback)}>
         <input name='firstName' placeholder="First Name" onChange={handleChange} />
         <input name='lastName' placeholder="Last Name" onChange={handleChange} />
         <button>Save</button>
       </form>
     </>
   );
-}
+};
 
 export default App;
 
---
+++ client/src/hooks/useForm.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+export default function useForm(defaults = {}){
+  const [values, setValues] = useState(defaults);
+
+  function handleChange(event){
+    setValues({...values, [event.target.name]: event.target.value});
+  }
+
+  function handleSubmit(event, callback){
+    event.preventDefault();
+    event.target.reset();
+    if(callback){
+      callback(values);
+    }
+  }
+
+  return [values, handleChange, handleSubmit];
+};
